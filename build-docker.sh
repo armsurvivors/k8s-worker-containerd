02f33d7f75145ae741e02f9bf44bfb3d11cd353a
Ricardo Pardini <ricardo@pardini.net>
2025-12-13 01:39:37 +0100

    allow extra args to build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -2,7 +2,7 @@
 set -e
 
 rm -rf ./out
-#docker buildx build --progress=plain --platform=linux/amd64 --pull --build-arg BASE_IMAGE=debian:trixie --build-arg OS_ARCH=amd64 -t containerd:amd64 .
-docker buildx build --progress=plain --platform=linux/arm64 --pull --build-arg BASE_IMAGE=debian:trixie --build-arg OS_ARCH=arm64 -t containerd:arm64 .
+#docker buildx build --progress=plain --platform=linux/amd64 --pull --build-arg BASE_IMAGE=debian:trixie --build-arg OS_ARCH=amd64 -t containerd:amd64  "${@}" .
+docker buildx build --progress=plain --platform=linux/arm64 --pull --build-arg BASE_IMAGE=debian:trixie --build-arg OS_ARCH=arm64 -t containerd:arm64 "${@}" .
 docker cp $(docker create --rm containerd:arm64):/out ./
 ls -lah ./out/
